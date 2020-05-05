bb89ea9def8e41bc641c9ca3fa0d79168e8eb98f
Nicola Toscan <nicolatoscan99@outlook.it>
2020-05-05 20:30:23 +0000

    Q first version

--- src/Q.c
+++ src/Q.c
@@ -3,96 +3,146 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include "common.h"
 
-#define MAXFILESIZE 1024
+#define INPUT_FILE STDIN_FILENO
+#define OUTPUT_FILE STDOUT_FILENO
 
-typedef struct Analysis_s
-{
-	int tot;
-	int letters;
-	int numbers;
-	int spaces;
-	int punctuaction;
-	int other;
-
-} Analysis;
-
-Analysis newAnalysis();
+int readSectionNumber();
+int readMySection(int max);
+int readNumberOfFiles();
+void analyseFiles(int numberOfFiles, Analysis *a);
 void analyseChar(char c, Analysis *a);
-void printAnalysis(Analysis *a);
 
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
-	{
-		printf("Usare: Q <section-to-read> <number-of-sections>\n\n");
-		exit(0);
-	}
+	int sections = 1;
+	int mySection = 1;
+	int nrOfFile;
 
-	if (argc != 3)
-	{
-		fprintf(stderr, "Error: 2 parameters where expected, but %d where found\n\n", argc);
-		exit(1);
-	}
-
-	Analysis analysis = newAnalysis();
-
-	char filename[MAXFILESIZE];
 	while (1)
 	{
-		fgets(filename, MAXFILESIZE, stdin);
-		int len = strlen(filename);
-		if (filename[len - 1] == '\n')
-			filename[len - 1] = '\0';
-		filename[MAXFILESIZE - 1] = '\0';
-		FILE *fp;
-		fp = fopen(filename, "r");
-		if (fp <= 0)
+		char cmd = readFirstChar(INPUT_FILE);
+		switch (cmd)
 		{
-			fprintf(stderr, "Error reading file\n");
-			printf("N\n");
-		}
-		else
-		{
-			while (!feof(fp))
-			{
-				int c = fgetc(fp);
-				analyseChar(c, &analysis);
-			}
-			printf("Y\n");
-			printAnalysis(&analysis);
+
+		case Q_FILE_COMMAND:
+			nrOfFile = readNumberOfFiles();
+			Analysis a = initAnalysis();
+			analyseFiles(nrOfFile, &a);
+			printAnalysis(&a);
+			break;
+
+		case Q_NUMBER_OF_SECTIONS_COMMAND:
+			sections = readSectionNumber();
+			break;
+
+		case Q_MY_SECTION_COMMAND:
+			mySection = readMySection(sections);
+			break;
+
+		case Q_QUIT_COMMAND:
+			exit(0);
+			break;
+
+		default:
+			error("Unknown command");
+			break;
 		}
 	}
 
 	return 0;
 }
 
-Analysis newAnalysis()
+int readSectionNumber()
 {
-	Analysis analysis;
-	analysis.tot = 0;
-	analysis.letters = 0;
-	analysis.numbers = 0;
-	analysis.spaces = 0;
-	analysis.punctuaction = 0;
-	analysis.other = 0;
-	return analysis;
+	int M = readNumber(INPUT_FILE);
+	if (M <= 0)
+	{
+		error("Number not valid");
+		return 1;
+	}
+
+	return M;
+}
+
+int readMySection(int max)
+{
+	int N = readNumber(INPUT_FILE);
+	if (N <= 0)
+	{
+		error("Number not valid");
+		return 1;
+	}
+
+	if (N > max)
+	{
+		error("Number out of range");
+		return 1;
+	}
+
+	return N;
+}
+
+int readNumberOfFiles()
+{
+	int N = readNumber(INPUT_FILE);
+	if (N <= 0) {
+		error("Number not valid");
+		return 0;
+	}
+	return N;
+}
+
+void analyseFiles(int numberOfFiles, Analysis *a)
+{
+	char fileName[MAX_FILENAME_LENGHT];
+	char buffer[128];
+
+	int i = 0;
+	for (i = 0; i < numberOfFiles; i++)
+	{
+		int nr = readline(INPUT_FILE, fileName, MAX_FILENAME_LENGHT);
+		if (nr < 0)
+		{
+			error("Filename too long");
+			continue;
+		}
+
+		int fd = open(fileName, O_RDONLY);
+		if (fd < 0)
+		{
+			error("Impossible to open");
+			continue;
+		}
+
+		int rd;
+		while (rd = read(fd, buffer, 127))
+		{
+			if (rd > 0)
+				buffer[rd] = '\0';
+
+			int j = 0;
+			for (j = 0; j < rd; j++)
+			{
+				analyseChar(buffer[j], a);
+			}
+		}
+	}
 }
 
 void analyseChar(char c, Analysis *a)
 {
-	a->tot++;
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		a->letters++;
-	else if ((c >= '1' && c <= '9'))
+	else if (c >= '1' && c <= '9')
 		a->numbers++;
 	else if (c == ' ')
 		a->spaces++;
+	else if (c == '.' || c == ',' || c == ':' || c == ';' || c == '?' || c == '!')
+		a->punctuaction++;
 	else
 		a->other++;
-}
 
-void printAnalysis(Analysis *a)
-{
-	printf("%d, %d, %d, %d, %d, %d\n", a->tot, a->letters, a->numbers, a->spaces, a->punctuaction, a->other);
+	a->tot++;
 }
