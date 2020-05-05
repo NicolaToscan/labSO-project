a794c174435c996f9bcbbe5c3a953fc498658609
Nicola Toscan <nicolatoscan99@outlook.it>
2020-05-05 20:30:04 +0000

    Common functions added

--- src/common.c
+++ src/common.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
+#include <stdio.h>
+#include "common.h"
 
-int readline(int file, char *buffer, int maxsize)
+int readline(const int file, char *buffer, const int maxsize)
 {
 	int i = 0;
 	while (i < maxsize)
@@ -16,5 +18,71 @@ int readline(int file, char *buffer, int maxsize)
 		}
 		i++;
 	}
+	buffer[maxsize - 1] = '\0';
 	return -1;
+}
+
+char readFirstChar(const int file)
+{
+	char CH[2];
+	readline(file, CH, 2);
+	return CH[0];
+}
+
+int readNumber(int file)
+{
+	char number[10];
+	if (readline(file, number, 10) == -1)
+	{
+		error("Input too big");
+		return 1;
+	}
+
+	return atoi(number);
+}
+
+void error(char msg[])
+{
+	fprintf(stderr, "\033[31;1m ERROR: %s \033[0m\n", msg);
+}
+
+Analysis initAnalysis()
+{
+	Analysis a;
+	a.letters = 0;
+	a.numbers = 0;
+	a.other = 0;
+	a.punctuaction = 0;
+	a.spaces = 0;
+	a.tot = 0;
+	return a;
+}
+
+void printAnalysis(Analysis *a)
+{
+	fprintf(stdout, "%d %d %d %d %d %d\n",
+			a->letters,
+			a->numbers,
+			a->other,
+			a->punctuaction,
+			a->spaces,
+			a->tot);
+
+}
+
+Analysis readAnalysis(int file)
+{
+	char buffer[60];
+	readline(file, buffer, 60);
+
+	Analysis a;
+	char *pEnd;
+	a.letters = strtol(buffer, &pEnd, 10);
+	a.numbers = strtol(pEnd, &pEnd, 10);
+	a.other = strtol(pEnd, &pEnd, 10);
+	a.punctuaction = strtol(pEnd, &pEnd, 10);
+	a.spaces = strtol(pEnd, &pEnd, 10);
+	a.punctuaction = strtol(pEnd, NULL, 10);
+
+	return a;
 }
--- src/common.h
+++ src/common.h
@@ -9,4 +9,19 @@ typedef struct Analysis_s
 
 } Analysis;
 
-int readline(int file, char *buffer, int maxsize);
+
+#define Q_FILE_COMMAND 'F'
+#define Q_NUMBER_OF_SECTIONS_COMMAND 'M'
+#define Q_MY_SECTION_COMMAND 'N'
+#define Q_QUIT_COMMAND 'Q'
+
+#define MAX_FILENAME_LENGHT 1024
+
+
+int readline(const int file, char *buffer, const int maxsize);
+char readFirstChar(const int file);
+void error(char msg[]);
+int readNumber(int file);
+Analysis initAnalysis();
+void printAnalysis(Analysis *a);
+Analysis readAnalysis(int file);
