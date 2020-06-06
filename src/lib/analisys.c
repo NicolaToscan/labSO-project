0d2a247a4767c0889eaf9c66a1a8f6871667f84b
Lorenzo Godi <34336808+LorenzoGodi@users.noreply.github.com>
2020-06-06 12:41:28 +0200

    Calcolo Analisi

--- src/lib/analisys.c
+++ src/lib/analisys.c
@@ -14,12 +14,62 @@ Analysis initAnalysis()
 
 void printAnalysis(const int file, Analysis a)
 {
-    write(file, a.val, sizeof(int));
+
+
+
+
+    //write(file, a.val, sizeof(int));
 }
 
 Analysis readAnalysis(const int file)
 {
-    Analysis a;
-    read(file, a.val, sizeof(int));
-    return a;
+
+
+
+
+    
+    // Analysis a;
+    // read(file, a.val, sizeof(int));
+    // return a;
 }
+
+void intToStr(ulong value, char str[ULONG_MAXLEN])
+{
+    int i;
+    for(i = 0; i < ULONG_MAXLEN; i++)
+    {
+        str[i] = '0';
+    }
+
+    int index = ULONG_MAXLEN - 1;
+    while(value > 0)
+    {
+        str[index--] = '0' + (value % 10);
+        value /= 10;
+    }
+}
+
+void strToInt(char str[ULONG_MAXLEN], ulong *value)
+{
+    ulong ul = 0;
+    int index = 0;
+    char c[1];
+
+    while(index < ULONG_MAXLEN)
+    {
+        c[0] = str[index++];
+        ul = (ul * 10) + atoi(c);
+    }
+
+    *value = ul;
+}
+
+
+bool isText(char c)         { return c >= ' ' && c <= '~'; }
+bool isUppLetter(char c)    { return c >= 'A' && c <= 'Z'; }
+bool isLowLetter(char c)    { return c >= 'a' && c <= 'z'; }
+bool isNumber(char c)       { return c >= '0' && c <= '9'; }
+bool isMathSymbol(char c)   { return strstr("=<>+-*/", c) != NULL; }
+bool isPunctuation(char c)  { return strstr(".,:;'!?`\"", c) != NULL; }
+bool isBracket(char c)      { return strstr("()[]{}", c) != NULL; }
+bool isSpace(char c)        { return c == ' '; }
--- src/lib/analisys.h
+++ src/lib/analisys.h
@@ -1,8 +1,36 @@
+typedef unsigned long ulong;
+#define ULONG_MAXLEN 7
+
+#define DIVCHAR '#'
+
 typedef struct Analysis_s
 {
-	int val;
+	ulong uppLetters;
+	ulong lowLetters;
+
+	ulong numbers;
+	ulong mathSymbols;
+
+	ulong punctuaction;
+	ulong brackets;
+	ulong spaces;
+
+	ulong otherText;
+	ulong other;
 } Analysis;
 
 Analysis initAnalysis();
-void printAnalysis(Analysis a);
+void intToStr(ulong value, char str[ULONG_MAXLEN]);
+void strToInt(char str[ULONG_MAXLEN], ulong *value);
+
+void printAnalysis(const int file, Analysis a);
 Analysis readAnalysis(const int file);
+
+bool isText(char c);
+bool isUppLetter(char c);
+bool isLowLetter(char c);
+bool isNumber(char c);
+bool isMathSymbol(char c);
+bool isPunctuation(char c);
+bool isBracket(char c);
+bool isSpace(char c);
